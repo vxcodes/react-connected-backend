158bbfda3161eb85a6e38ff24fae512fbcae1357
Viraj Patel <pnviraj@gmail.com>
2021-06-04 11:39:17 -0700

    wip: nested models

--- models/post.js
+++ models/post.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    content: String,
+}, { timestamps: true });
+
 const postSchema = new Schema({
     post: String,
+    comments: [commentSchema]
 }, { timestamps: true });
+
+const followSchema = new Schema ({
+    fullName: String,
+    email: String,
+    following: Boolean,
+})
+
+const userSchema = new Schema({
+    fullName: String,
+    email: String,
+    follows: [followSchema]
+}, { timestamps: true })
+
 
 module.exports = mongoose.model('Post', postSchema);
