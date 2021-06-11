e41d54d1aa78c99785c4396b3fd13f42b5957f0f
Viraj Patel <pnviraj@gmail.com>
2021-06-10 22:28:04 -0700

    wip: changes model

--- models/post.js
+++ models/post.js
@@ -1,58 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const commentSchema = new Schema({
-//     content: String,
-// }, { timestamps: true });
+const commentSchema = new Schema({
+    content: String,
+}, { timestamps: true });
 
 const postSchema = new Schema({
-    userName: {
-        type: String,
-    },
-    title: {
-        type: String,
-    },
+    // userName: {
+    //     type: String,
+    // },
+    // title: {
+    //     type: String,
+    // },
     post: {
         type: String,
     },
-    // comments: [commentSchema]
+    comments: [commentSchema]
 }, { timestamps: true });
 
-// const userSchema = new Schema({
-//     userName: {
-//         type: String,
-//     },
-//     email: {
-//         type: String,
-//     },
-//     followers: {
-//         type: Array,
-//         default: []
-//     },
-//     following: {
-//         type: Array,
-//         default: []
-//     },
-// }, { timestamps: true })
-
-
-// const userSchema = new Schema({
-//     userName: {
-//         type: String,
-//     },
-//     email: {
-//         type: String,
-//     },
-//     followers: {
-//         type: Array,
-//         default: []
-//     },
-//     following: {
-//         type: Array,
-//         default: []
-//     },
-//     posts: [postSchema]
-// }, { timestamps: true })
 
 
 module.exports = mongoose.model('Post', postSchema);
