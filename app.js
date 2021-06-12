fa2cf7ebf1b7cbb1f51accad7c23b12eabacded6
Viraj Patel <pnviraj@gmail.com>
2021-06-11 20:55:22 -0700

    wip: final prep for deployment

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 var express = require('express');
 var logger = require('morgan');
 var cors = require('cors');
@@ -5,8 +7,6 @@ var postsRouter = require('./routes/posts');
 
 
 var app = express();
-
-require('dotenv').config()
 require('./config/database')
 
 app.use(logger('dev'));
